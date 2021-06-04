6393d9feef4acc80af06b39d33443f719be346e6
Jacob Marks <jacobjmarks@outlook.com>
2021-06-05 09:49:24 +1000

    Move login page url to config

--- src/SharesightApiInterface.ts
+++ src/SharesightApiInterface.ts
@@ -3,7 +3,7 @@ import axiosCookieJarSupport from "axios-cookiejar-support";
 import { JSDOM } from "jsdom";
 import * as qs from "qs";
 import { CookieJar } from "tough-cookie";
-import { SHARESIGHT_API_BASEURL } from "./config";
+import { SHARESIGHT_API_BASEURL, SHARESIGHT_LOGIN_PAGE } from "./config";
 
 export interface InterfaceContext {
   agent: AxiosInstance;
@@ -20,15 +20,13 @@ export class SharesightApiInterface {
   }
 
   public async login(username: string, password: string): Promise<SharesightApiInterface> {
-    const LOGIN_PAGE = "https://portfolio.sharesight.com/users/sign_in";
-
-    const { data: loginPageHtml } = await this.ctx.agent.get<string>(LOGIN_PAGE);
+    const { data: loginPageHtml } = await this.ctx.agent.get<string>(SHARESIGHT_LOGIN_PAGE);
     const loginPageDoc = new JSDOM(loginPageHtml).window.document;
     const authTokenInput = loginPageDoc.querySelector<HTMLInputElement>("input[name=authenticity_token]");
     const authToken = authTokenInput.value;
 
     const loginResponse = await this.ctx.agent.post(
-      LOGIN_PAGE,
+      SHARESIGHT_LOGIN_PAGE,
       qs.stringify({
         authenticity_token: authToken,
         "user[email]": username,
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,1 +1,2 @@
+export const SHARESIGHT_LOGIN_PAGE = "https://portfolio.sharesight.com/users/sign_in";
 export const SHARESIGHT_API_BASEURL = "https://api.sharesight.com/api";
